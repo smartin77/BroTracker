82151982a22ce6ccc111d65cdc7a756ff100edad
smartin77 <smartinpo@gmail.com>
2026-09-10 21:38:42 +0200

    Add AudioEngine skeleton

---
+++ src/core/audio_engine.cpp
@@ -0,0 +1,16 @@
+#include "audio_engine.h"
+
+void AudioEngine::Initialize()
+{
+    Reset();
+}
+
+void AudioEngine::Reset()
+{
+    processed_samples_ = 0;
+}
+
+void AudioEngine::Process(uint32_t sample_count)
+{
+    processed_samples_ += sample_count;
+}
